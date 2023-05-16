6d4ca48fea4b4604bcebedf19b09bf051b30ec88
Jhon Gonzalez <66422237+jhongonzalezweb@users.noreply.github.com>
2023-05-16 01:12:39 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,4 +1,12 @@
-var seconds = 60; 
+var seconds = 6; 
+var song = 'audio.mp3' ;
+var audio = document.getElementById('song');
+audio.src = song
+
+function reproducir() {
+    audio.play();
+}
+
 function secondPassed() { 
   var minutes = Math.round((seconds - 30)/60); 
   var remainingSeconds = seconds % 60; 
@@ -8,11 +16,13 @@ function secondPassed() {
   document.getElementById('countdown').innerHTML = minutes + ":" +     remainingSeconds; 
   if (seconds == 0) { 
     clearInterval(countdownTimer); 
-    // alert('SE ACABAO'); 
+    // alert('SE ACABAO');
     document.getElementById('countdown').innerHTML = "Rest"; 
+    reproducir()
   } else { 
     seconds--; 
   } 
 } 
 
 var countdownTimer = setInterval('secondPassed()', 1000);
+
