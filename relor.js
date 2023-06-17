e7ca02ae1346e938084dfd9b5de60a8e31e2722d
Jhon Gonzalez <66422237+jhongonzalezweb@users.noreply.github.com>
2023-06-17 14:52:50 -0400

    Update relor.js

--- relor.js
+++ relor.js
@@ -71,7 +71,7 @@
 			m.classList.add("stylesOrg")
 		}
 
-		if (segundos == 00) {
+		if (horas == 00) {
 			// location.reload();
 			var r = Math.floor(Math.random() * 10);
 			var d = `'images/img_tree (${r}).jpg'`;
